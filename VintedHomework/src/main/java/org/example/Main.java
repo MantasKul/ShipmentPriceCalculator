84366a977c270abb831f271ec0ebcf01773e23d9
Mantas <m.kulikauskas01@gmail.com>
2023-08-25 17:36:31 +0300

    Created empty project

---
+++ VintedHomework/src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+/*
+    Read file
+    Check if line syntax is correct
+    keep track of month (for discount rules)
+    add price to the line
+    subtract price by following the rules
+ */
