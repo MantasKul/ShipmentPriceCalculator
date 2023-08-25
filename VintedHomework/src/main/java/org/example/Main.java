02a01ba754d1eca99e79a3d2875db18857f590f0
Mantas <m.kulikauskas01@gmail.com>
2023-08-25 18:23:37 +0300

    Add read input.txt from resource folder

--- VintedHomework/src/main/java/org/example/Main.java
+++ VintedHomework/src/main/java/org/example/Main.java
@@ -1,14 +1,34 @@
 package org.example;
 
+import java.io.File;
+import java.net.URL;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        readFile();
+    }
 
+    public static void readFile() {
+        try {
+            URL url = Main.class.getClassLoader().getResource("input.txt");
+            File file = new File(url.toURI());//new File(System.getProperty("user.dir") + "\\src\\main\\resources\\input.txt");
+
+            Scanner sc = new Scanner(file);
+
+            while(sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to find a file");
+        }
     }
 }
 
 /*
     Read file
-    Check if line syntax is correct
+    Check if line syntax/pattern of line is correct
     keep track of month (for discount rules)
     add price to the line
     subtract price by following the rules
